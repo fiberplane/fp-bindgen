5456a67a1ab63de26b1217f67f86136a880df36e
Benno van den Berg <benno@fiberplane.com>
2021-09-20 14:29:05 +0200

    Fix test

--- example-protocol/src/assets/rust_wasmer_runtime_test/expected_bindings.rs
+++ example-protocol/src/assets/rust_wasmer_runtime_test/expected_bindings.rs
@@ -1,8 +1,9 @@
 use super::types::*;
+use crate::errors::InvocationError;
 use crate::{
     support::{
         assign_async_value, create_future_value, export_to_guest, import_from_guest,
-        resolve_async_value, FatPtr, InvocationError, ModuleFuture, FUTURE_STATUS_READY,
+        resolve_async_value, FatPtr, ModuleFuture, FUTURE_STATUS_READY,
     },
     Runtime, RuntimeInstanceData,
 };
