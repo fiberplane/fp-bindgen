a6b539922051e0fb84562f99518ef59335d8f7ce
Simon Rasmussen <zetlon@gmail.com>
2021-10-20 14:46:49 +0200

    Fix doc tests

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -307,6 +307,7 @@ pub fn fp_export_signature(_attributes: TokenStream, input: TokenStream) -> Toke
 ///
 /// Example usage of implementing a `log` function of a `logger` provider:
 /// ```
+/// use macros::fp_export_impl; //this would be `logger::fp_export_impl` inside the plugin crate
 /// #[fp_export_impl(logger)]
 /// pub fn log(msg: String, foo: String) -> String {
 ///     format!("{} + {} => {0}{1}", msg, foo)
