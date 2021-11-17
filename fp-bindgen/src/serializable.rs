3fd66dfacf4d5ff0e137de0d263369bc0aa582a6
Arend van Beelen jr <arendjr@gmail.com>
2021-11-17 18:02:27 +0100

    Support rename attr on enum variants

--- fp-bindgen/src/serializable.rs
+++ fp-bindgen/src/serializable.rs
@@ -5,7 +5,7 @@ use once_cell::sync::Lazy;
 use crate::CustomType;
 use crate::{
     generics::{contains_generic_arg, specialize_type_with_dependencies},
-    types::{EnumOptions, GenericArgument, Variant},
+    types::{EnumOptions, GenericArgument, Variant, VariantAttrs},
     Type,
 };
 use std::{
@@ -266,11 +266,13 @@ where
                     name: "Ok".to_owned(),
                     ty: Type::Tuple(vec![Type::named_generic("T")]),
                     doc_lines: vec![" Represents a succesful result.".to_owned()],
+                    attrs: VariantAttrs::default(),
                 },
                 Variant {
                     name: "Err".to_owned(),
                     ty: Type::Tuple(vec![Type::named_generic("E")]),
                     doc_lines: vec![" Represents an error.".to_owned()],
+                    attrs: VariantAttrs::default(),
                 },
             ],
             EnumOptions::default(),
