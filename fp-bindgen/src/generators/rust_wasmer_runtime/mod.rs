0d37a20f9764baebebcea8bd8d72e2a504f21b98
Roy Jacobs <roy.jacobs@gmail.com>
2022-11-30 15:45:41 +0100

    Revert "Only use singlepass compiler (#139)", since this doesn't work yet on M1.
    This reverts commit dba48e6769d3be7d65980d6a2dff34d1b2f9a594.

--- fp-bindgen/src/generators/rust_wasmer_runtime/mod.rs
+++ fp-bindgen/src/generators/rust_wasmer_runtime/mod.rs
@@ -350,13 +350,21 @@ impl Runtime {{
         let module = Module::new(&store, wasm_module)?;
         Ok(Self {{ module }})
     }}
-
+    
+    #[cfg(any(target_arch = "arm", target_arch = "aarch64"))]
     fn default_store() -> wasmer::Store {{
-        let compiler = wasmer::Singlepass::default();
-        let engine = wasmer::Universal::new(compiler).engine();
+        let compiler = wasmer_compiler_cranelift::Cranelift::default();
+        let engine = wasmer_engine_universal::Universal::new(compiler).engine();
         Store::new(&engine)
     }}
-
+    
+    #[cfg(not(any(target_arch = "arm", target_arch = "aarch64")))]
+    fn default_store() -> wasmer::Store {{
+        let compiler = wasmer_compiler_singlepass::Singlepass::default();
+        let engine = wasmer_engine_universal::Universal::new(compiler).engine();
+        Store::new(&engine)
+    }}
+    
     {exports}
 }}
 
