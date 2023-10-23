68c09154121061ff0bcc63d5226b4caf57ad1620
Arend van Beelen jr. <arend@arendjr.nl>
2023-10-23 16:38:40 +0200

    Fix Clippies about partial_cmp()

--- fp-bindgen/src/functions.rs
+++ fp-bindgen/src/functions.rs
@@ -97,7 +97,7 @@ impl Ord for Function {
 
 impl PartialOrd for Function {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.name.partial_cmp(&other.name)
+        Some(self.cmp(other))
     }
 }
 
